a2ab8a188ea8506b010fb7e436f110234fce73d8
Ekow Buadu <ekow.buadu@yale.edu>
2019-05-22 14:42:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,10 @@
 def square_array(array)
   new_numbers = []
+  counter = 0
   array.each do |element|
     result = element * element
+    new_numbers[counter] = result
+    counter = counter + 1
   end
   return
 end
